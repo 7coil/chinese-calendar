00775ba99a3f02caad4323df1a327a266e899ec1
Leondro Lio <github@leondrolio.com>
2025-01-04 03:42:35 +0000

    Remove unused side image

--- src/components/SideImage.tsx
+++ src/components/SideImage.tsx
@@ -1,5 +1,3 @@
-import Image from "./SideImage.png";
-
 const SideImage = () => (
   <div style={{ backgroundColor: "lightgrey", width: "100%", height: "100%" }}>
     <span>Insert advert here</span>
